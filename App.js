37ec6c82e8c8db901d542312bd5f86b73edcfbe7
DADM <daiverandres99@gmail.com>
2019-05-15 23:13:05 -0500

    Revert "1111"
    This reverts commit 643763e2afeba8124d9107218b4da80a60a84560.

--- App.js
+++ App.js
@@ -3,20 +3,11 @@ import firebase from 'firebase';
 import { Provider } from 'react-redux';
 import store from './src/store';
 import AppScreenContainer from './src/screens/container';
+import firebaseconfig from './firebaseConfig';
 
 export default class App extends React.Component {
   componentDidMount() {
-    const firebaseConfig = {
-      apiKey: 'AIzaSyCU2idxtGjyKUyLILLVWeLn1qmswRD--wE',
-      authDomain: 'inv-app-cfce0.firebaseapp.com',
-      databaseURL: 'https://inv-app-cfce0.firebaseio.com',
-      projectId: 'inv-app-cfce0',
-      storageBucket: 'inv-app-cfce0.appspot.com',
-      messagingSenderId: '484861582270',
-      appId: '1:484861582270:web:0a25102b370b0df4'
-    };
-    
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseconfig);
   }
   render() {
     return (
